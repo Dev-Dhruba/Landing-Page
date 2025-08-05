c41985376d9041947dcc4c98d6c192cc6b8ab4c9
Dhruba Bhattacharyya <ece2023114@rcciit.org.in>
2025-08-06 02:08:07 +0530

    navbar highlight logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
 
 const Navbar = () => {
-
+  const [activeLink, setActiveLink] = useState("Home");
   const links = ["Home", "AI Tools", "Testimonials", "Blogs", "Pricing"];
 
   return (
     <nav className='flex justify-around py-2 w-screen'>
       <img src="/logo.png" alt="Nivyr logo" className='h-12'/>
-      <ul className='flex w-146 justify-between items-center text-white font-semibold]'>
+      <ul className='flex w-146 justify-between items-center text-white font-semibold'>
         {links.map((link, index) => (
           <li
             key={index}
-            className={`hover:text-[#FFD300] cursor-pointer transition-colors`}
+            className={`cursor-pointer transition-colors ${
+              activeLink === link 
+                ? 'text-yellow-400' 
+                : 'hover:text-[#FFD300]'
+            }`}
+            onClick={() => setActiveLink(link)}
           >
             {link}
           </li>
